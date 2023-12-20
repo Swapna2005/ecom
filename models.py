655ff966840fe0b50e8e127ed36c713dde79336e
Swapna2005 <146418921+Swapna2005@users.noreply.github.com>
2023-12-20 11:09:57 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+
+
+
+# class User(models.Model):
+#   firstname = models.CharField(max_length=255)
+#   lastname = models.CharField(max_length=255)
+
+# class login(models.Model):
+#   username = models.CharField(max_length=255)
+#   password = models.CharField(max_length=255)
